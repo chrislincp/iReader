ace9a6f302bed3299729302eed9caffbb4854bd9
林昌鹏 <chrislincp@163.com>
2018-07-04 13:08:57 +0800

    fix http

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,54 +1,24 @@
 import axios from 'axios';
 import qs from 'qs';
-import { getSex } from './utils';
+import { getSex, convertObject } from './utils';
 import Store from '../store';
 
 const httpFactory = method => async (url, params = {}) => {
-  console.log(Store.common.sex)
+  // console.log(Store.common.sex, Store.userInfo)
   return new Promise((resove, reject) => {
-    if (Store.common.sex) {
-      let opts = Object.assign({}, params, {sex: Store.common.sex});
-      console.log(url, opts);
-
-      if (method == 'Get') {
-        axios.get(url, opts).then(res => {
-          console.log(res.data);
-          resove(res.data);
-        }).catch(err => {
-          reject(res);
-        })
-      } else if (method == 'Post') {
-        axios.post(url, qs.stringify(opts)).then(res => {
-          console.log(res.data);
-          resove(res.data);
-        }).catch(err => {
-          console.log(err);
-          reject(err);
-        })
-      }
-    } else {
-      getSex().then(sex => {
-        let opts = Object.assign({}, params, {sex});
-        console.log(url, opts);
-
-        if (method == 'Get') {
-          axios.get(url, opts).then(res => {
-            console.log(res.data);
-            resove(res.data);
-          }).catch(err => {
-            reject(res);
-          })
-        } else if (method == 'Post') {
-          axios.post(url, qs.stringify(opts)).then(res => {
-            console.log(res.data);
-            resove(res.data);
-          }).catch(err => {
-            console.log(err);
-            reject(err);
-          })
-        }
-      })
-    }
+    const {common, userInfo} = Store;
+    let others = {userkey: userInfo.userkey, userid: userInfo.userid, sex: common.sex};
+    let type = method == 'Get' ? 'get' : 'post';
+    let opts = Object.assign({}, others, params);   
+    console.log(url, opts);
+    opts = type == 'get' ? opts : qs.stringify(opts);
+    axios[type](url, opts).then(res => {
+      console.log(res.data);
+      resove(res.data);
+    }).catch(err => {
+      console.log(err);
+      reject(err);
+    })
   })
 }
 const Get = httpFactory('Get');
